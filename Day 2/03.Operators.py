36370ef73b5b9ad9651110d1d08c43cf23b95d27
Devashish Mayur Potnis <99007076+devashishpotnis@users.noreply.github.com>
2026-01-28 11:12:53 +0530

    Add files via upload

---
+++ Day 2/03.Operators.py
@@ -0,0 +1,15 @@
+""""
+Operators in python
+Arithmetic operators (+ - * / %)
+Comparison operators (== != > < >= <=)
+Logical operators (and or not)
+"""
+
+# Arithmetic operators (+ - * / %)
+a=int(input("Enter 1st Number : "))
+b=int(input("Enter 2nd Number : "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
---
+++ Day 2/04.Conditional statement.py
@@ -0,0 +1,13 @@
+""""
+Conditional statement
+"""
+
+a=10
+b=20
+
+if a>b:
+    print("a is bigger")
+else:
+    print("b is bigger")
+
+
