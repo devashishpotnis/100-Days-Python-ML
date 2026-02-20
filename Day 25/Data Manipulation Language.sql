d8842b8eeb298f1f06a0b16c5f1d80379f4eced6
Devashish Mayur Potnis <99007076+devashishpotnis@users.noreply.github.com>
2026-02-20 21:57:51 +0530

    Add files via upload

---
+++ Day 25/Data Manipulation Language.sql
@@ -0,0 +1,11 @@
+-- INSERT
+INSERT INTO students VALUES (1, 'Devashish', 22);
+
+-- UPDATE
+UPDATE students
+SET name = 'Anand'
+WHERE id = 1;
+
+-- DELETE
+DELETE FROM students
+WHERE id = 1;
---
+++ Day 25/Data Query Language.sql
@@ -0,0 +1,9 @@
+-- Select all data
+SELECT * FROM students;
+
+-- Select specific columns
+SELECT name, age FROM students;
+
+-- With condition
+SELECT * FROM students
+WHERE age > 20;
